07f88ee918dab0490ebcc088563b13da2a3578fc
kamkara <lnclass.kamkara@gmail.com>
2021-09-04 15:01:03 +0000

    add membership with custom fields

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+protected
+    # If you have extra params to permit, append them to the sanitizer.
+    def configure_permitted_parameters
+    sign_up_params = [ :first_name, :last_name, :full_name, :matricule, :contact, :city, :role, :email, :password, :slug, :serie_bac, :status_bac, :school, :school_name, :gender,  :status_payment]
+    
+      devise_parameter_sanitizer.permit :sign_up, keys: sign_up_params
+      devise_parameter_sanitizer.permit :account_update, keys: sign_up_params
+      devise_parameter_sanitizer.permit :sign_in, keys: [:logged, :password]
+    end 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,36 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+     ################## VALIDATES  ###############
+  validates :first_name, :last_name, :full_name, :matricule, :email, :contact, :role,  presence: true
+  validates :full_name,  length: { minimum:5 }
+  validates :contact,  length: { minimum:10 }
+  validates :matricule,  length: { minimum:9 }
+
+
+ ############# customize fields###############"" 
+def email
+  self.email = "#{self.contact}@gmail.com"
+end
+
+  def password
+   self.password = "#{self.matricule}" 
+  end
+
+  
+  def full_name
+    self.full_name = "#{self.first_name} #{self.last_name}"
+  end
+  def slug
+    self.slug = self.full_name
+  end
+
+     ################## SLUG ###############
+  extend FriendlyId
+    friendly_id :full_name, use: :slugged
+
+  def should_generate_new_friendly_id?
+    full_name_changed?
+  end
 end
