385d4747be4f34aad6774561fb54bee3a890c62d
kamkara <lnclass.kamkara@gmail.com>
2021-09-06 17:34:53 +0000

    add course editor

--- app/models/course.rb
+++ app/models/course.rb
@@ -19,6 +19,7 @@ class Course < ApplicationRecord
   Course.all.with_rich_text_content # Preload the body without attachments.
   Course.all.with_rich_text_content_and_embeds # Preload both body and attachments.
 
+  
   #SLUG
   extend FriendlyId
     friendly_id :title, use: :slugged
