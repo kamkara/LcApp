89143f866caf1143ee8d9ad0da942a0eb7da096b
kamkara <lnclass.kamkara@gmail.com>
2021-09-07 17:38:18 +0000

    sign up basic

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get 'admin/dashboard'
   get 'admin/table'
   root to:'home#index'
-  devise_for :users
   
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
