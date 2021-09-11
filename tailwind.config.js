97fc6f22a67de918c3c57d7d2afb8fc12d58dc17
kamkara <lnclass.kamkara@gmail.com>
2021-09-11 16:42:29 +0000

    add bgackground image on home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,9 @@ module.exports = {
   presets: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    backgroundImage: {
+      'bgImg': "url("<%= image_tag "img-bachelier-2021.jpeg" %>")",
+    },
     screens: {
       sm: '640px',
       md: '768px',
