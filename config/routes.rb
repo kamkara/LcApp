81ea8c07b282406c72648bfb6ecd750f11aa3fcf
kamkara <lnclass.kamkara@gmail.com>
2021-09-15 12:07:51 +0000

    add team sign up form

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,25 @@
 Rails.application.routes.draw do
-  resources :courses
-  resources :city_areas
-  resources :levels
-  resources :materials
+  resources :courses, :city_areas, :levels, :materials
+ 
+  get 'team', to:'partner#team'
+  get 'professeur', to:'partner#partner'
   get 'admin/index'
   get 'admin/create'
   get 'admin/dashboard'
   get 'admin/table'
   root to:'home#index'
-  
-  devise_for :users
+    devise_scope :user do
+    get 'profile/edit'    => 'devise/registrations#edit',   :as => :edit_user_registration
+    get 'profile/cancel'  => 'devise/registrations#cancel', :as => :cancel_user_registration
+  end
+
+  devise_for  :users,
+              :path => '',
+              :path_names => {  :sign_in =>       'connexion',
+                                :sign_out =>      'logout',
+                                :sign_up =>       '',
+                                :registration =>  'inscription',
+                                :edit =>          'edit',
+                                 }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
